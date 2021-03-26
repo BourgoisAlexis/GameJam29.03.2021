9a877223edc0c92dcd0ff42f9c301b7a862a2e77
BourgoisAlexis <bourgoisalexis@gmail.com>
2021-03-26 19:29:16 +0100

    first commit

---
+++ Assets/Editor/FastRename.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class FastRename : EditorWindow
+{
+    #region Variables
+    public GameObject Parent;
+    public string NewName;
+
+    private Editor editor;
+    private Vector2 scrollPosition;
+    #endregion
+
+    [MenuItem("Tools/FastRename")]
+    public static void ShowWindow()
+    {
+        GetWindow<FastRename>("Fast Rename");
+    }
+
+    private void OnGUI()
+    {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, false, false);
+
+        if (!editor) { editor = Editor.CreateEditor(this); }
+        if (editor) { editor.OnInspectorGUI(); }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Rename", GUILayout.Width(150), GUILayout.Height(20)))
+            Rename();
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void Rename()
+    {
+        Transform[] childs = Parent.GetComponentsInChildren<Transform>();
+
+        if (childs.Length > 1)
+            Parent.name = NewName + "s";
+        else 
+            Parent.name = NewName;
+
+        for (int i = 1; i < childs.Length; i++)
+        {
+            string n = NewName + "_";
+            if (i - 1 < 10)
+                n += "0";
+
+            n += i - 1;
+
+            childs[i].gameObject.name = n;
+        }
+    }
+}
